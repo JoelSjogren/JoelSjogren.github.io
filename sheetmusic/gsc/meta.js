027d73eaa6907f041dc96c97e25e53ed9ab1631c
Joel Sjögren <joelsjogren.wii@gmail.com>
2018-12-30 13:56:35 +0100

    add many filename exceptions

--- sheetmusic/gsc/meta.js
+++ sheetmusic/gsc/meta.js
@@ -13,6 +13,30 @@ var gsc = {
 	var regex1 = RegExp('Music_([a-zA-Z0-9]*)', 'g');
 	var x;
 	while ((x = regex1.exec(asm)) !== null) {
+	    var exceptions = {
+		'Title': 'TitleScreen',
+		'KantoGymLeaderBattle': 'KantoGymBattle',
+		'Heal': 'HealPokemon',
+		'WildVictory': 'WildPokemonVictory',
+		'GymVictory': 'GymLeaderVictory',
+		'PokemonTalk': 'ProfOaksPokemonTalk',
+		'RivalAfter': 'AfterTheRivalFight',
+		'KimonoEncounter': 'LookKimonoGirl',
+		'JohtoGymLeaderBattle': 'JohtoGymBattle',
+		'ProfElm': 'ElmsLab',
+		'BugCatchingContestRanking': 'ContestResults',
+		
+	    };
+	    var encounters = [
+		'Hiker', 'Lass', 'Officer', 'Rival',
+		'Youngster', 'Beauty', 'Rocket', 'Pokemaniac',
+		'Sage'
+	    ];
+	    for (var i = 0; i < encounters.length; i++) {
+		var e = encounters[i];
+		exceptions[e + 'Encounter'] = 'Look' + e;
+	    }
+	    x[1] = exceptions[x[1]] || x[1];
 	    add({
 		'name': x[1],
 		'thumb': 'gsc/thumb/' + x[1].toLowerCase() + '.png',
