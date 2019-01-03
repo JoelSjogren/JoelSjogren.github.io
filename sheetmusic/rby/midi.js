da3534e7828f8b31ec6d03307562e771871ada7f
Joel Sjögren <joelsjogren.wii@gmail.com>
2019-01-03 15:21:54 +0100

    adapt gsc parser to rby (fixes gym theme)

--- sheetmusic/rby/midi.js
+++ sheetmusic/rby/midi.js
@@ -7,64 +7,215 @@ var rby = {
 
     configure: function(args) {
 	var regex1 = /piece=([a-zA-Z0-9]*)&channels=([a-zA-Z,0-9]*)/;
-	tmp = regex1.exec(args);
+	var tmp = regex1.exec(args);
 
 	rby.piece = tmp[1];
 	rby.channels = tmp[2].split(',');
 	
 	var fnam = rby.piece.toLowerCase();
 	rby.path = `rby/audio/music/${fnam}.asm`;
+	
     },
 
+    load: function(a) {
+	var b = rby.lex(a);
+	var c = rby.parse(b);
+	var d = rby.compile(c);
+	return d;
+    },
 
-    load: function(asm) {
-	var chan = [];
-	for (var i = 0; i < rby.channels.length; i++) {
-	    chan.push(rby.loadChannel(asm, i));
+    lex: function(a) {
+	var b = a.split('\n');
+	var c = [];  // nonempty lines
+	for (var i = 0; i < b.length; i++) {
+	    var d = rby.lexLine(b[i]);
+	    if (d !== undefined) {
+		c.push(d);
+	    }
 	}
-	return {name: `Music_${rby.piece}`, chan: chan}
+	return c;
     },
 
-    // to be improved...
-    loadChannel: function(text, i) {
-	var lines = text.split('\n');
-	var state = 'looking';
-	var goal = 'Music_' + rby.piece + '_' + rby.channels[i];
-	var octave;
-	var timeunit;
-	var boot = [];
-	var loop = [];
-	for (var j = 0; j < lines.length; j++) {
-	    if (state == 'looking' && lines[j].startsWith(goal)) {
-		state = 'copying';
-	    } else if (state == 'copying') {
-		if (lines[j].startsWith('\tnotetype')) {
-		    var start = 1 + 'notetype'.length+1;
-		    var end = lines[j].indexOf(',');
-		    timeunit = parseInt(lines[j].slice(start, end));
+    parse: function(a) {
+	var b = [];  // blocks
+	var c = undefined;  // current block;
+	for (var i = 0; i < a.length; i++) {
+	    if (a[i][0] == 'block') {
+		if (c !== undefined) {
+		    b.push(c);
 		}
-		if (lines[j].startsWith('\toctave')) {
-		    octave = Number(lines[j][1+'octave'.length+1]);
+		c = [];
+	    }
+	    if (a[i][0][0].toUpperCase() === a[i][0][0]) {
+		c.push(['note', a[i][0], a[i][1]]);
+	    } else if (a[i][0] == 'rest') {
+		c.push(['note', '__', a[i][1]]);
+	    } else {
+		c.push(a[i]);
+	    }
+	}
+	if (c !== undefined) {
+	    b.push(c);
+	}
+	return b;
+    },
+
+    compile: function(a) {
+	var name = rby.piece;
+
+	var dbw = rby.makeDBW(a);
+	var chan = [];
+	for (var i = 0; i < dbw.length; i++) {
+	    var c = rby.compileChannel(a, dbw[i]);
+	    if (c !== undefined) {
+		chan.push(c);
+	    }
+	}
+
+	return {
+	    name: name,
+	    chan: chan
+	}
+    },
+
+    lexLine: function(a) {
+	if (a.length == 0) {
+	    return undefined;
+	}
+	
+	var k = a.indexOf(':');
+	if (k != -1) {
+	    return ['block', a.slice(0, k)];
+	}
+
+	if (a[0] == '\t') {
+	    var i = a.indexOf(' ');
+	    var head;
+	    var args;
+	    if (i == -1) {
+		head = a.slice(1);
+		args = [];
+	    }
+	    else {
+		head = a.slice(1, i);
+		args = a.slice(i + 1).split(', ');
+	    }
+	    return [head].concat(args);
+	}
+    },
+
+    extractLabel: function(a) {
+	var b = a.slice(6);
+	return b.slice(0, b.indexOf('_'));
+    },
+
+    makeDBW: function() {
+	return rby.channels.map(l => `Music_${rby.piece}_${l}`);
+    },
+
+    compileChannel: function(a, id) {
+	/* Model of the coding of channels: (a bit outdated) (never perfect)
+	    - there is an initial block
+	    - if a block ends with 'loopchannel',
+	      that will determine the periodicity
+	    - 'callchannel' can always be inlined
+	    - nested inlining never occurs
+	    - the current octave is a global variable
+	*/
+
+	var events = [];
+	var outline = {};
+	
+	var ids = a.map(function(aj) { return aj[0][1]; });
+	for (var i = ids.indexOf(id); i < a.length; i++) {
+	    console.log(`chan_id=${id}, i=${i}, block_id=${ids[i]}`);
+	    outline[ids[i]] = events.length;
+	    for (var j = 1; j < a[i].length; j++) {
+		var line = a[i][j];
+		if (line[0] == 'notetype') {
+		    rby.timeunit = parseInt(line[1], 10);
 		}
-		var k = chromatic.indexOf(lines[j].slice(1, 3));
-		if (k != -1) {
-		    var dt = Number(lines[j].split(' ')[1]);
-		    loop.push([12*octave + k, dt*timeunit])
+		if (line[0] == 'octave') {
+		    rby.octave = parseInt(line[1]);
 		}
-		if (lines[j].startsWith('\trest')) {
-		    var dt = Number(lines[j].split(' ')[1]);
-		    loop.push([undefined, dt*timeunit]);
+		if (line[0] == 'note') {
+		    events.push(rby.extractNote(line));
 		}
-		if (lines[j].startsWith('\tloopchannel')) {
-		    break;
+		if (line[0] == 'callchannel') {
+		    var k = ids.indexOf(line[1]);
+		    events = events.concat(rby.inline(a[k]));
 		}
-		if (lines[j].indexOf('::') != -1) {
-		    boot = boot.concat(loop);
-		    loop = [];
+		if (line[0] == 'loopchannel') {
+		    var times = parseInt(line[1]);
+		    var target = line[2];
+		    //console.log(line);
+		    var k = outline[target];
+		    //console.log(k);
+		    if (k === undefined) {
+			return;
+		    }
+		    var loop = events.slice(k);
+		    //console.log(`len=${loop.length}`);
+		    if (times == 0) {
+			var boot = events.slice(0, k);
+			return {boot: boot, loop: loop};
+		    } else {
+			for (var m = 1; m < times; m++) {
+			    events = events.concat(loop);
+			}
+		    }
+		}
+		if (line[0] == 'endchannel') {
+		    return {boot: events, loop: []};
 		}
 	    }
 	}
-	return  {boot: boot, loop: loop};
+    },
+
+    chromatic: [
+	'C_', 'C#', 'D_', 'D#', 'E_',
+	'F_', 'F#', 'G_', 'G#', 'A_', 'A#', 'B_'
+    ],
+
+    octave: undefined,
+
+    extractNote: function(line) {
+//	console.log(line);
+//	throw "break";
+	var tone = line[1];
+	var abs;
+	if (tone != '__') {
+	    abs = 12 * rby.octave;
+	    abs += rby.chromatic.indexOf(tone);
+	}
+	var duration = parseInt(line[2]) * rby.timeunit;
+	return [abs, duration];
+    },
+
+    inline: function(a) {
+	var b = [];
+	for (var i = 1; i < a.length; i++) {
+	    var line = a[i];
+	    if (line[0] == 'notetype') {
+		rby.timeunit = parseInt(line[1].slice(1), 16);
+	    }
+	    if (line[0] == 'octave') {
+		rby.octave = parseInt(line[1]);
+	    }
+	    if (line[0] == 'note') {
+		b.push(rby.extractNote(line));
+	    }
+	    if (line[0] == 'loopchannel') {
+		var times = parseInt(line[1]);
+		var target = line[2];
+		if (target !== a[0][1]) return;
+		var loop = b.slice(0);
+		for (var m = 1; m < times; m++) {
+		    b = b.concat(loop);
+		}
+	    }
+	}
+	return b;
     }
 
 }
